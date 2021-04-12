77dc0029c5bc537b063ac3cef982981e28621961
Ayushi <ayushi.sharma@apisero.com>
2021-04-12 10:19:19 +0530

    Second Commit

--- Demo.java
+++ Demo.java
@@ -2,6 +2,5 @@ class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello, I am Line No 1");
 		System.out.println("Hello, I am Line No 2");
-		System.out.println("Hello, I am Line No 3");
 	}
 }
